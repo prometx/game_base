50f019ffe027914a2e3697dc2901001533d43603
prometx <prometx@gmail.com>
2016-02-25 11:39:27 -0500

    Added files via upload
    updating char_dbase.cpp

--- char_dbase.cpp
+++ char_dbase.cpp
@@ -17,11 +17,25 @@ struct char_dbase {
 int main () {
 
 	char_dbase character;
-	dbase *char_ptr;
+	char_dbase *char_ptr;
 
 	char_ptr = &character;
+	
+	character.str = 13;
+	character.health = 100;
+	character.dex = 19;
+	character.age = 37;
+	character.name = Lothar;
+	character.race = Human;
 
-	cout << char_ptr->str;
+
+	cout << char_ptr -> str << "\n\n";
+	cout << char_ptr-> health << "\n\n";
+	cout << char_ptr-> dex << "\n\n";
+	cout << char_ptr-> age << "\n\n";
+	cout << char_ptr-> name << "\n\n";
+	cout << char_ptr-> race << "\n\n";
+
 
 	cin.get();
 
